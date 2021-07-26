bb27abbac3b5514b5b88b47d6e6cb70fe924f6be
Ruchir <ruchir@pdx.edu>
2021-07-25 19:50:19 -0700

    Initial Commit

---
+++ UsingUI/UsingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingUIApp.swift
+//  UsingUI
+//
+//  Created by Ruchir on 7/25/21.
+//
+
+import SwiftUI
+
+@main
+struct UsingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
